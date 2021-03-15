e118c93a203db7c5c9a99036f9d0396acdd7e436
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-03-15 14:22:21 -0500

    finally got my 5 day to display

--- assests/script.js
+++ assests/script.js
@@ -3,7 +3,7 @@
 // Define variables
 var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
 var city = 'London';
-var apiKey = '&APPID=a9b4a8580c0d8f382ed6c59b947f9ed6';
+var apiKey = 'a9b4a8580c0d8f382ed6c59b947f9ed6';
 var units = '&units=imperial';
 var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q="
 var count = "&cnt=40"
@@ -18,12 +18,14 @@ var uvUrl = "api.openweathermap.org/data/2.5/uvi?lat="
 // the location is gathered from the user
 // the url is set 
 // the five day forcasts loads on the bottom of the page
+
+
 $('#submit').on('click', function(){
     console.log('buttonclick')
     $('.tbody').html("")
     var location = $('#query').val();
-    var url = api + location + apiKey + units;
-    var fiveDay = fiveDayUrl + location + apiKey + units + count;
+    var url = api + location + "&APPID=" + apiKey + units;
+    var fiveDay = fiveDayUrl + location + "&APPID=" + apiKey + units + count;
     
     loadJson(url)
     loadFiveDayForcast(fiveDay)
@@ -58,36 +60,74 @@ function loadJson(url){
 
 
         $(".city").text(cityName)
-        $(".temp").text(temperature)
+        $(".temp").html(`${temperature}&deg;`)
         $(".humidity").text(humidity)
         $(".wind-speed").text(wind)
         $(".high").text(high)
         $(".low").text(low)
         // $(".uv").text(uvIndex)
+
+        getFiveDay(lat, long)
+
     })
 }
 
-// function getUvIndex(lat, lon)
-//     queryUrl = "uvIndex" + lat + "&lon=" + lon + "apiKey"
-//     $.ajax({
-//         url: queryUrl,
-//         method: "GET",
-//     }).then(function (weatherData) {
-//         var uvColor = response.value;
-//         if (uvColor > 0 && uvColor < 2){
-//             uvSpan = "green";
-//         } else if (uvColor > 2 && uvColor < 6){
-//             uvSpan = "yellow";
-//         } else if (uvColor > 6 && uvColor < 8){
-//             uvSpan = "orange";
-//         } else ig (uvColor > 8 && uvColor < 11){
-//             uvSpan = "purple";
-//         }
 
-//         $("<span>").attr("id", uvSpan).text(uvColor).appendTo("#uvIndex");
+function getFiveDay(lat, long){
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly&appid=${apiKey}`;
+    $.get(url).done(function(data){
+        console.log(data)
+        getUvIndex(data.current.uvi)
+
+        data.daily.forEach((day,index)=>{
+            if(index > 0 && index < 6){
+                let container = document.getElementById("fiveday");
+                let date = document.createElement("div")
+                let temp = document.createElement("div")
+                let card = document.createElement("div")
+                let high = document.createElement("div")
+                let low = document.createElement("div")
+
+                card.appendChild(date)
+                card.appendChild(temp)
+                card.appendChild(high)
+                card.appendChild(low)
+
+                container.appendChild(card)
+                date.innerHTML=new Date(day.dt * 1000).toLocaleDateString("en-US");
+                temp.innerHTML= day.temp.day
+                high.innerHTML= day.temp.max
+                low.innerHTML= day.temp.min
+
+                date.classList.add("date")
+                temp.classList.add("temp")
+            }
+
+        })
+
+    })
+
+}
+
+function getUvIndex(index){
+
+    let uvSpan = ""
+    
+        var uvColor = index;
+        if (uvColor > 0 && uvColor < 2){
+            uvSpan = "green";
+        } else if (uvColor > 2 && uvColor < 6){
+            uvSpan = "yellow";
+        } else if (uvColor > 6 && uvColor < 8){
+            uvSpan = "orange";
+        } else if (uvColor > 8 && uvColor < 11){
+            uvSpan = "purple";
+        }
+
+        $("#uv").attr("class", uvSpan).text(uvColor)
      
 
-//     }
+    }
     
 
 
@@ -101,33 +141,33 @@ function loadJson(url){
 // Tbody method used to gather 5 day forcast
 // I noticed that the forcast for each day is
 // chaned every three hours and used that time
-// stamp to parse the data to the page
+// stamp to parse the data to the page with a for loop
 
 
-function loadFiveDayForcast(url){
-    $.get(url).done(function(weatherData){
+// function loadFiveDayForcast(url){
+//     $.get(url).done(function(weatherData){
        
        
-        // Populates table and insert a row for each forecast
-        var forecasts = weatherData.list;
-        for (var i = 0; i < forecasts.length; i++) {
-            var forecast = forecasts[i];
-            var date = new Date(forecast.dt * 1000).toLocaleDateString("en-US");
-            var time = new Date(forecast.dt * 1000).toLocaleTimeString("en-US");
-            var row = `
-         <tr>
-         <th scope="row">${date + ' ' + time}</td>
-            <td>${forecast.main.temp}</td>
-            <td>${forecast.main.humidity}</td>
-            <td>${forecast.wind.speed}</td>
-            <td>${forecast.main.temp_max}</td>
-            <td>${forecast.main.temp_min}</td>
-          </tr>
-          `
-          $('.tbody').append(row);
-        }
+//         // Populates table and insert a row for each forecast
+//         var forecasts = weatherData.list;
+//         for (var i = 0; i < forecasts.length; i++) {
+//             var forecast = forecasts[i];
+//             var date = new Date(forecast.dt * 1000).toLocaleDateString("en-US");
+//             var time = new Date(forecast.dt * 1000).toLocaleTimeString("en-US");
+//             var row = `
+//          <tr>
+//          <th scope="row">${date + ' ' + time}</td>
+//             <td>${forecast.main.temp}</td>
+//             <td>${forecast.main.humidity}</td>
+//             <td>${forecast.wind.speed}</td>
+//             <td>${forecast.main.temp_max}</td>
+//             <td>${forecast.main.temp_min}</td>
+//           </tr>
+//           `
+//           $('.tbody').append(row);
+//         }
 
-        return;
+//         return;
 
-    })    
-}
+//     })    
+// }
